ca49c54a8e058b20b9511309545567364149287b
Tetyuev Pavel <jetexe2@gmail.com>
2018-04-03 15:06:21 +0500

    Added repair methods reference support

---
+++ src/References/RepairMethods/RepairMethodsEntry.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AvtoDev\StaticReferences\References\RepairMethods;
+
+use Illuminate\Support\Str;
+use AvtoDev\StaticReferences\References\AbstractReferenceEntry;
+
+/**
+ * Class RepairMethodsEntry.
+ *
+ * Сущность типа "Метод ремонта".
+ */
+class RepairMethodsEntry extends AbstractReferenceEntry
+{
+    /**
+     * Коды метода ремонта.
+     *
+     * @var string[]|array
+     */
+    protected $codes;
+
+    /**
+     * Описание метода ремонта.
+     *
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configure($input = [])
+    {
+        if (is_array($input)) {
+            foreach ($input as $key => $value) {
+                switch ($key = Str::lower((string) $key)) {
+                    // Коды регистрационного действия
+                    case 'codes':
+                        $value       = ! is_array($value)
+                            ? explode(',', (string) $value)
+                            : $value;
+                        $this->codes = array_filter(array_map(function ($item) {
+                            return (string) ($item);
+                        }, (array) $value));
+                        break;
+
+                    // Описание регистрационного действия
+                    case 'description':
+                        $this->description = (string) trim((string) $value);
+                        break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Возвращает описание метода ремонта.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Возвращает коды метода ремонта.
+     *
+     * @return string[]|array
+     */
+    public function getCodes()
+    {
+        return $this->codes;
+    }
+}
---
+++ tests/References/RepairMethodsReferenceTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AvtoDev\StaticReferences\Tests\References;
+
+use Illuminate\Support\Str;
+use AvtoDev\StaticReferences\References\RepairMethods\RepairMethods;
+
+/**
+ * Class RepairMethodsReferenceTest.
+ *
+ * @group repair_methods
+ */
+class RepairMethodsReferenceTest extends AbstractReferenceTestCase
+{
+    /**
+     * @var RepairMethods
+     */
+    protected $instance;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function testArrayKeys()
+    {
+        foreach (['codes', 'description'] as $key_name) {
+            $this->assertArrayHasKey($key_name, $this->instance->first()->toArray());
+        }
+    }
+
+    /**
+     * Тест базовых акцессоров данных.
+     *
+     * @return void
+     */
+    public function testBasicData()
+    {
+        $this->assertGreaterThan(18, count($this->instance->all()));
+        $assert_with = 'Частичная замена';
+
+        /*
+         * По кодам.
+         */
+        // На латинице
+        $this->assertEquals($assert_with, $this->instance->getByCode('ET')->getDescription());
+        $this->assertNull($this->instance->getByCode(' ET '));
+
+        $this->assertFalse($this->instance->hasCode(' ET '));
+
+        /*
+         * По описаниям.
+         */
+        $description       = 'Частичная замена';
+        $description_short = 'ичная';
+        $assert_with       = 'ET';
+
+        $this->assertContains($assert_with, $this->instance->getByDescription($description)->getCodes());
+        $this->assertContains($assert_with, $this->instance->getByDescription(' ' . $description)->getCodes());
+        $this->assertContains($assert_with, $this->instance->getByDescription($description_short)->getCodes());
+        $this->assertContains($assert_with, $this->instance->getByDescription(Str::upper($description))->getCodes());
+
+        $this->assertTrue($this->instance->hasDescription($description));
+        $this->assertTrue($this->instance->hasDescription($description_short));
+        $this->assertFalse($this->instance->hasDescription('ЫDfsdgfs dsfDFfds'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getReferenceClassName()
+    {
+        return RepairMethods::class;
+    }
+}
