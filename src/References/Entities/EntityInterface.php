26df99f740fec622d1135473cff4afd6eb0d92d8
Dmitry Peichev <peichev.d@spectrumdata.ru>
2024-05-24 14:56:32 +0500

    Laravel 11 support was added

--- src/References/Entities/EntityInterface.php
+++ src/References/Entities/EntityInterface.php
@@ -2,6 +2,11 @@
 
 namespace AvtoDev\StaticReferences\References\Entities;
 
+use Illuminate\Contracts\Support\Arrayable;
+
+/**
+ * @extends Arrayable<string, mixed>
+ */
 interface EntityInterface extends \Illuminate\Contracts\Support\Arrayable
 {
     //
